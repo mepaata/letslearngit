c80907a27aec0bb2012490ccfaa9555f8674568a
Mayur <mepaata25@gmail.com>
2025-12-20 13:25:23 +0530

    add sum.java

---
+++ sum.java
@@ -0,0 +1,9 @@
+class Addition {
+    public static void main(String[] args){
+        int a = 11;
+        int b = 22;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
