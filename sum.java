c738661e202721909e14159cbcfc30a2bec8fc68
Mayur <mepaata25@gmail.com>
2025-12-27 13:58:25 +0530

    add implement

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 class Addition {
     public static void main(String[] args){
-        int a = 11;
-        int b = 22;
+        int a = 31;
+        int b = 12;
         int sum = a + b;
 
         System.out.println("Sum = " + sum);
